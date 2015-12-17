5e2c19a26482820aa32e782c9e8aa2193b44b1c8
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2015-12-17 15:31:23 +0800

    replace default viewcontroller implemention

--- XY-iOS-Demo/XY-iOS-Demo/MainViewController.h
+++ XY-iOS-Demo/XY-iOS-Demo/MainViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  XY-iOS-Demo
 //
 //  Created by hzyuxiaohua on 15/12/17.
 //  Copyright © 2015年 X Co., Ltd. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <XLForm/XLForm.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : XLFormViewController
 
 @end
-
---
+++ XY-iOS-Demo/XY-iOS-Demo/MainViewController.m
@@ -0,0 +1,28 @@
+//
+//  MainViewController.m
+//  XY-iOS-Demo
+//
+//  Created by hzyuxiaohua on 15/12/17.
+//  Copyright © 2015年 X Co., Ltd. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self installBasicUIElements];
+}
+
+- (void)installBasicUIElements
+{
+    self.title = @"welcome";
+}
+
+@end
